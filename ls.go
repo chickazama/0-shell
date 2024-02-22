a0d5325301c0c4384e13820bbfea31aee03614ea
Matthew Hope <chickazama@gmail.com>
2024-02-22 17:09:44 +0000

    Implement a flag on ls command

--- ls.go
+++ ls.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/fs"
 	"log"
 	"os"
+	"strings"
+)
+
+var (
+	LS_CMD = flag.NewFlagSet("ls", flag.ContinueOnError)
+	LS_A   = LS_CMD.Bool("a", false, "include hidden files and directories")
 )
 
 func ls(args []string) {
+	if len(args) > 1 {
+		err := LS_CMD.Parse(args[1:])
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	}
+	// fmt.Println(*LS_A)
 	dir, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err.Error())
@@ -16,8 +30,15 @@ func ls(args []string) {
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	for _, ent := range dirent {
-		fmt.Printf("%s  ", ent.Name())
+	if *LS_A {
+		fmt.Print(".  ..  ")
 	}
+	for _, ent := range dirent {
+		name := ent.Name()
+		if !(strings.HasPrefix(name, ".") && !*LS_A) {
+			fmt.Printf("%s  ", ent.Name())
+		}
+	}
+	*LS_A = false
 	fmt.Println()
 }
