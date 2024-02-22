0e89e4942f9241279e3a8fe707739c66c126a1d2
Matthew Hope <chickazama@gmail.com>
2024-02-22 18:41:14 +0000

    Handle recursive ls flag

--- ls.go
+++ ls.go
@@ -6,36 +6,86 @@ import (
 	"io/fs"
 	"log"
 	"os"
+	"slices"
 	"strings"
 )
 
-var (
-	LS_CMD = flag.NewFlagSet("ls", flag.ContinueOnError)
-	LS_A   = LS_CMD.Bool("a", false, "include hidden files and directories")
-)
-
-func ls(args []string) {
-	if len(args) > 1 {
-		err := LS_CMD.Parse(args[1:])
+func ls(fields []string) {
+	cmd := flag.NewFlagSet("ls", flag.ContinueOnError)
+	all := cmd.Bool("a", false, "include hidden files and directories")
+	reverse := cmd.Bool("r", false, "reverse listing")
+	recursive := cmd.Bool("R", false, "recursive listing")
+	if len(fields) > 1 {
+		err := cmd.Parse(fields[1:])
 		if err != nil {
-			log.Fatal(err.Error())
+			fmt.Println(err.Error())
+			return
 		}
 	}
-	// fmt.Println(*LS_A)
-	dir, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err.Error())
+	argv := cmd.Args()
+	var fsys fs.FS
+	switch len(argv) {
+	case 0:
+		fsys = os.DirFS("./")
+	case 1:
+		fsys = os.DirFS(argv[0])
 	}
-	entries, err := fs.ReadDir(os.DirFS(dir), ".")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	for _, e := range entries {
-		name := e.Name()
-		if !(strings.HasPrefix(name, ".") && !*LS_A) {
-			fmt.Printf("%s  ", name)
+	if *recursive {
+		var sb strings.Builder
+		start := "."
+		var output []string
+		err := fs.WalkDir(fsys, start, func(path string, d fs.DirEntry, err error) error {
+			if d.IsDir() {
+				if !(strings.HasPrefix(path, ".") && !*all && path != ".") {
+					add := fmt.Sprintf("%s:\n", path)
+					sb.WriteString(add)
+					entries, err := fs.ReadDir(fsys, path)
+					if err != nil {
+						log.Fatal(err.Error())
+					}
+					if *reverse {
+						slices.Reverse(entries)
+					}
+					for _, e := range entries {
+						name := e.Name()
+						if !(strings.HasPrefix(name, ".") && !*all) {
+							add = fmt.Sprintf("%s  ", name)
+							sb.WriteString(add)
+						}
+					}
+					output = append(output, sb.String())
+					sb.Reset()
+				}
+			}
+			return nil
+		})
+		if *reverse {
+			slices.Reverse(output)
 		}
+		for _, s := range output {
+			fmt.Println(s)
+		}
+		if err != nil {
+			log.Println(err.Error())
+		}
+	} else {
+		entries, err := fs.ReadDir(fsys, ".")
+		if err != nil {
+			fmt.Printf("ls: unable to list contents of '%s'\n", argv[0])
+			return
+		}
+		if *reverse {
+			slices.Reverse(entries)
+		}
+		for _, e := range entries {
+			name := e.Name()
+			if !(strings.HasPrefix(name, ".") && !*all) {
+				fmt.Printf("%s  ", name)
+			}
+		}
+		fmt.Println()
 	}
-	*LS_A = false
-	fmt.Println()
+	*all = false
+	*reverse = false
+	*recursive = false
 }
