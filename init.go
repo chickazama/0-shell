02e50ed2b269432f865fa9a8652c9d4139761542
Matthew Hope <chickazama@gmail.com>
2024-02-22 14:59:54 +0000

    Exec underlying UNIX commands

--- init.go
+++ init.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"os"
 )
 
@@ -12,20 +11,10 @@ const (
 
 var (
 	HOME   string
-	LS_CMD *flag.FlagSet
-	LS_A   *bool
-	LS_r   *bool
 	reader *bufio.Reader
 )
 
 func init() {
-	setLS_CMD()
 	HOME = os.Getenv("HOME")
 	reader = bufio.NewReader(os.Stdin)
-}
-
-func setLS_CMD() {
-	LS_CMD = flag.NewFlagSet("ls", flag.ContinueOnError)
-	LS_A = LS_CMD.Bool("a", false, "list all")
-	LS_r = LS_CMD.Bool("r", false, "reverse")
 }
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"os"
@@ -9,30 +8,46 @@ import (
 	"strings"
 )
 
-const (
-	newline = '\n'
-)
-
-var (
-	HOME   string
-	reader *bufio.Reader
-)
-
-func init() {
-	HOME = os.Getenv("HOME")
-	reader = bufio.NewReader(os.Stdin)
-}
-
 func main() {
 	clear()
-	for {
+	loop := true
+	for loop {
 		prompt()
 		str, err := reader.ReadString(newline)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
-		fmt.Print(str)
+		loop = parse(str)
 	}
+}
+
+func parse(str string) bool {
+
+	fields := strings.Fields(str)
+	switch fields[0] {
+	case "cd":
+		if len(fields) > 1 {
+			err := os.Chdir(fields[1])
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+		} else {
+			err := os.Chdir(HOME)
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+		}
+	case "exit":
+		return false
+	default:
+		cmd := exec.Command(fields[0], fields[1:]...)
+		cmd.Stdout = os.Stdout
+		err := cmd.Run()
+		if err != nil {
+			log.Println(err.Error())
+		}
+	}
+	return true
 }
 
 func clear() {
@@ -51,7 +66,7 @@ func prompt() {
 	}
 	if strings.HasPrefix(dir, HOME) {
 		dir = strings.TrimPrefix(dir, HOME)
-		dir = fmt.Sprintf("~%s", dir)
+		dir = fmt.Sprintf("\033[0;32m~%s", dir)
 	}
 	fmt.Printf("%s$ ", dir)
 }
