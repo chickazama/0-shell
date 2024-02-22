d96dce81b995ab028eb338fe7a7a95c20814e04e
Matthew Hope <chickazama@gmail.com>
2024-02-22 17:00:11 +0000

    Implement cd command in execute loop

--- execute.go
+++ execute.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 	"strings"
 )
 
@@ -11,31 +9,15 @@ func execute(cmd string) bool {
 	fields := strings.Fields(cmd)
 	switch fields[0] {
 	case "ls":
-		ls(fields[1:])
+		ls(fields)
 	case "cd":
-		if len(fields) > 1 {
-			err := os.Chdir(fields[1])
-			if err != nil {
-				log.Fatal(err.Error())
-			}
-		} else {
-			err := os.Chdir(HOME)
-			if err != nil {
-				log.Fatal(err.Error())
-			}
-		}
+		cd(fields)
 	case "exit":
 		return false
 	case "clear":
 		clear()
 	default:
 		fmt.Printf("'%s' command not implemented.\n", fields[0])
-		// cmd := exec.Command(fields[0], fields[1:]...)
-		// cmd.Stdout = os.Stdout
-		// err := cmd.Run()
-		// if err != nil {
-		// 	log.Println(err.Error())
-		// }
 	}
 	return true
 }
